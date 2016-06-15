2359713ab7aea2555aae8e592e61f597ee81ac8b
dianaartiom <diana.artiom24@gmail.com>
2016-06-15 13:33:12 +0300

    Created a switch btw two fragments in a fragment placeholder

---
+++ LearnFragmengs/app/src/main/java/com/example/diana/learnfragmengs/FragmentTwo.java
@@ -0,0 +1,20 @@
+package com.example.diana.learnfragmengs;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by diana on 6/14/16.
+ */
+public class FragmentTwo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_fragment_two, container, false);
+    }
+}
